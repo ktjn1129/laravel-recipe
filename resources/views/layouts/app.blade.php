fa318c6339a47ec5a81c6386534e9a42dfa102e1
Jinichiro Koto <jinichirokoto@JinichironoMacBook-Pro.local>
2024-08-27 14:40:25 +0900

    add header and footer components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,13 @@
         <!-- Scripts -->
         <script src="https://cdn.tailwindcss.com"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="css/color.css">
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <body class="font-sans antialiased background-color">
+        <div class="min-h-screen">
+            <!-- 共通ヘッダーコンポーネント -->
+            @include('layouts.announce-header')
+            @include('layouts.global-header')
 
             <!-- Page Heading -->
             @if (isset($header))
@@ -33,5 +36,6 @@
                 {{ $slot }}
             </main>
         </div>
+        @include('layouts.footer')
     </body>
 </html>
