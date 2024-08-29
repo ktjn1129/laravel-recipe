a93402f97f4ee0d4fce3ca952dcee4415e6845a2
ktjn1129 <ktjn1129@gmail.com>
2024-08-29 18:29:23 +0900

    add filter to recipe page

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Recipe;
 use App\Models\Category;
+use Illuminate\Support\Facades\DB;
+
 class RecipeController extends Controller
 {
     public function home()
@@ -28,16 +30,34 @@ class RecipeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $recipes = Recipe::select('recipes.id', 'recipes.title', 'recipes.description', 'recipes.created_at', 'recipes.image', 'users.name')
-        ->join('users', 'users.id', '=', 'recipes.user_id')
-        ->orderBy('recipes.created_at', 'desc')
-        ->get();
+        $filters = $request->all();
+
+        $query = Recipe::query()->select('recipes.id', 'recipes.title', 'recipes.description', 'recipes.created_at', 'recipes.image', 'users.name', \DB::raw('AVG(reviews.rating) as rating'))
+            ->join('users', 'users.id', '=', 'recipes.user_id')
+            ->leftJoin('reviews', 'reviews.recipe_id', '=', 'recipes.id')
+            ->groupBy('recipes.id')
+            ->orderBy('recipes.created_at', 'desc');
+
+        if(!empty($filters['categories'])) {
+            $query->whereIn('recipes.category_id', $filters['categories']);
+        }
+
+        if(!empty($filters['rating'])) {
+            $query->havingRaw('AVG(reviews.rating) >= ?', [$filters['rating']])
+                ->orderBy('rating', 'desc');
+        }
+
+        if(!empty($filters['title'])) {
+            $query->where('recipes.title', 'like', '%'.$filters['title'].'%');
+        }
+
+        $recipes = $query->get();
 
         $categories = Category::all();
 
-        return view('recipes.index', compact('recipes', 'categories'));
+        return view('recipes.index', compact('recipes', 'categories', 'filters'));
     }
 
     /**
